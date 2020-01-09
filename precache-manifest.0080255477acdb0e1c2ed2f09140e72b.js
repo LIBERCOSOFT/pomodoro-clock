e7cf0c0d645f61699a3d2378570d2584f55436c9
OLADIPO-K <akinspreciousola@gmail.com>
2020-01-09 09:16:40 +0100

    Updates

---
+++ precache-manifest.0080255477acdb0e1c2ed2f09140e72b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a863839ac935de63a1f84ff7f967205c",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "aea79c6a2ffc368f4658",
+    "url": "/pomodoro-clock/static/css/main.7269a79e.chunk.css"
+  },
+  {
+    "revision": "9b3a5007987034dff77a",
+    "url": "/pomodoro-clock/static/js/2.652dea17.chunk.js"
+  },
+  {
+    "revision": "aea79c6a2ffc368f4658",
+    "url": "/pomodoro-clock/static/js/main.10dd60dd.chunk.js"
+  },
+  {
+    "revision": "aa817215ef5e490bad59",
+    "url": "/pomodoro-clock/static/js/runtime~main.647dc629.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/pomodoro-clock/static/media/logo.ee7cd8ed.svg"
+  }
+]);
